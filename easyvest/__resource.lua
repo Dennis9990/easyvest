c324ce9e51a81dd92fea4826850d045553e4a19e
Dennis9990 <69021024+Dennis9990@users.noreply.github.com>
2020-08-01 01:38:55 +0200

    Update __resource.lua

--- easyvest/__resource.lua
+++ easyvest/__resource.lua
@@ -1,1 +1,11 @@
-client_script "client.lua"
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'EasyVest Made by Dennis9990'
+
+client_scripts {
+	'client.lua'
+} 
+
+server_scripts {
+	'server.lua'
+}
